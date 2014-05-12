7c5d5683dbf011eb5048d93f70918ce2722d9e49
Eric Merritt <emmerrit@amazon.com>
2014-05-12 13:27:22 -0700

    fix line break in mode description

--- src/projmake-mode-pkg.el
+++ src/projmake-mode-pkg.el
@@ -2,6 +2,5 @@
 ;; Copyright (C) 2012 Eric Merritt
 ;;
 (define-package "projmake-mode" "0.0.10"
-  "Project oriented automatic builder and error
-highlighter, flymake for projects"
+  "Project oriented automatic builder and error highlighter, flymake for projects"
   '())
